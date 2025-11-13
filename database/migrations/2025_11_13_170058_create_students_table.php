9499726efa6dd9c152088084b622aa003554013d
wahajn <wahaj.nadeem@qannas.co>
2025-11-13 22:05:48 +0500

    added student migration

--- database/migrations/2025_11_13_170058_create_students_table.php
+++ database/migrations/2025_11_13_170058_create_students_table.php
@@ -11,12 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('students', function (Blueprint $table) {
-            //
-        });
-
-        Schema::table('students', function (Blueprint $table) {
-            $table->string('profile_image')->nullable();
+        Schema::create('students', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('email');
+            $table->string('phone');
+            $table->timestamps();
         });
     }
 
@@ -25,8 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::table('students', function (Blueprint $table) {
-            //
-        });
+        Schema::dropIfExists('students');
     }
 };
